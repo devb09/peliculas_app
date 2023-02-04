4554411253229a955d61a21b76f87524e10993c2
brn091 <brn091dev@gmail.com>
2023-02-03 19:57:22 -0500

    mive title and poster in details

--- lib/screens/details.screen.dart
+++ lib/screens/details.screen.dart
@@ -11,7 +11,10 @@ class DetailsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments?.toString() ?? 'no-movie';
     return Scaffold(
       body: CustomScrollView(
-        slivers: [_CustomAppBar()],
+        slivers: [
+          _CustomAppBar(),
+          SliverList(delegate: SliverChildListDelegate([_PosterAndTitle()]))
+        ],
       ),
     );
   }
@@ -43,6 +46,53 @@ class _CustomAppBar extends StatelessWidget {
           image: NetworkImage('https://via.placeholder.com/500x300'),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 200,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Movie.title',
+                  style: textTheme.headline5,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2),
+              Text('Movie.originalTitle',
+                  style: textTheme.subtitle1,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1),
+              Row(
+                children: [
+                  const Icon(Icons.star_outline, size: 15, color: Colors.grey),
+                  const SizedBox(width: 5),
+                  Text('movie.coreAverage',
+                      style: textTheme.caption)
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
