517a12b57ff38e2883af6cceaf2fba695ecd13d2
brn091 <brn091dev@gmail.com>
2023-02-07 21:53:07 -0500

    refactor movie provider

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -10,34 +10,36 @@ class MoviesProvider extends ChangeNotifier {
   final String _baseUrlBase = 'api.themoviedb.org';
   final String _apiKey = '590b4c7a0091dd534740300cc474f31e';
   final String _language = 'en-US';
-  final double _page = 1;
 
   List<Movie> onDisplayMovies = [];
   List<Movie> popularMovies = [];
+  int popularPage = 0;
 
   MoviesProvider() {
     getPlayinNowMovies();
     getPopularMovies();
   }
 
-  getPlayinNowMovies() async {
-    var url = Uri.https(_baseUrlBase, '3/movie/now_playing',
-        {'api_key': _apiKey, 'language': _language, 'page': '$_page'});
+  Future<String> _getJsonData(String endpoint, {page = 1}) async {
+    var url = Uri.https(_baseUrlBase, endpoint,
+        {'api_key': _apiKey, 'language': _language, 'page': '$page'});
     final response = await http.get(url);
-    final movieResponse = NowPlayingResponse.fromRawJson(response.body);
+    return response.body;
+  }
+
+  getPlayinNowMovies() async {
+    final jsonData = await _getJsonData('/3/movie/now_playing');
+    final movieResponse = NowPlayingResponse.fromRawJson(jsonData);
     onDisplayMovies = movieResponse.results;
 
     notifyListeners();
   }
 
   getPopularMovies() async {
-    var url = Uri.https(_baseUrlBase, '3/movie/popular',
-        {'api_key': _apiKey, 'language': _language, 'page': '$_page'});
-    final response = await http.get(url);
-    final movieResponse = NowPlayingResponse.fromRawJson(response.body);
+    popularPage++;
+    final jsonData = await _getJsonData('/3/movie/popular', page: popularPage);
+    final movieResponse = NowPlayingResponse.fromRawJson(jsonData);
     popularMovies = [...popularMovies, ...movieResponse.results];
-    print('popularMovies');
-    print(popularMovies);
     notifyListeners();
   }
 }
