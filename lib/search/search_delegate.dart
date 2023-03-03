52b488030eed5bafb4091092ef27c9e4da9edc3b
brn091 <brn091dev@gmail.com>
2023-03-03 10:11:35 -0500

    feat: hero animation in search delegate

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -78,12 +78,18 @@ class _movieItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    movie.heroID = 'search-${movie.id}';
+
     return ListTile(
-      leading: FadeInImage(
-        placeholder: AssetImage('assets/no-image.jpg'),
-        image: NetworkImage(movie.fullPosterImg),
-        width: 50,
-        fit: BoxFit.contain,
+      leading: Hero(
+        tag: movie.heroID!,
+        child: FadeInImage(
+          placeholder: AssetImage('assets/no-image.jpg'),
+          image: NetworkImage(movie.fullPosterImg),
+          width: 50,
+          fit: BoxFit.contain,
+        ),
       ),
       title: Text(movie.title),
       subtitle: Text(movie.originalTitle),
